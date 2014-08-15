5e68e615f0f3cb32640bbf9f7d4c9c4ccc02a5af
Michel Ferreira da Silva Melo <michel.ferreira.melo@gmail.com>
2014-08-14 22:28:20 -0300

    Update Cifra_de_Cesar.py

--- Cifra_de_Cesar.py
+++ Cifra_de_Cesar.py
@@ -1,1 +1,33 @@
-Class Cesar:
+# -*- coding: utf-8 -*-
+class Cesar:
+   
+   def __init__(self):
+    self.letras='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+   #Metodo que criptografa
+   def crip(self,texto_normal,chave=3):
+      texto_cifrado= ' '
+      texto_normal=texto_normal.upper()
+            
+      for i in texto_normal:
+         if i in self.letras:
+            pos=self.letras.find(i)+chave                
+            if pos >= 26:
+               pos -= 26
+            texto_cifrado += self.letras[pos]      
+      return texto_cifrado
+
+   #Metodo que descriptografa 
+   def decrip(self, texto_cifrado, chave=-3):
+      texto_normal=' '
+      texto_cifrado=texto_cifrado.upper()
+      
+      for i in texto_cifrado:
+         if i in self.letras:
+            pos=self.letras.find(i)+chave
+            texto_normal += self.letras[pos]
+      return texto_normal
+
+
+  
+      
